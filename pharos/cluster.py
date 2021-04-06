2553d2901fc8677f9d6dde4a29a56e9ae51ab260
Yiling-J <njjyl723@gmail.com>
2021-04-06 12:40:42 +0800

    remove unused file

--- pharos/cluster.py
+++
@@ -1,9 +0,0 @@
-from kubernetes import config
-
-
-class Cluster:
-    def __init__(self, k8s_config):
-        self.api_client = config.new_client_from_config(context=k8s_config)
-
-    class Meta:
-        support_models = []
