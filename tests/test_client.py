56632c37a3470dcc7dd7cbad321383d9a2dc2e72
Yiling-J <njjyl723@gmail.com>
2021-04-09 21:57:57 +0800

    update test

---
+++ tests/test_client.py
@@ -0,0 +1,10 @@
+from unittest import TestCase, mock
+from pharos.client import Client
+
+
+class ClientTestCase(TestCase):
+
+    @mock.patch('pharos.client.kubernetes')
+    def test_client_with_settings(self, k8s_mock):
+        client = Client('test', disable_compress=True)
+        self.assertEqual(client.settings.disable_compress, True)
